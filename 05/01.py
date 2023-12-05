6e4940e89289185c2748d370df14cccd75358567
unknown <k@rclient.net>
2023-12-05 17:39:17 +0100

    05 - init

---
+++ 05/01.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+#
+# Advent of Code 2023
+# Day 5, part 1
+# https://github.com/krcs/aoc-2023
+#
+input = "./input.txt"
+
+def read_lines(file):
+    result = []
+
+    with open(input,'r') as f:
+        while True:
+            line = f.readline()
+
+            if len(line)>0:
+                result.append(line.strip())
+
+            if not line:
+                break
+    return result
+
+lines = read_lines(input)
+
+test = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+lines = test.split('\n')
+
+print(lines)
